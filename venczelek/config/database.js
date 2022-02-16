1ba5dac898dedd47888fb0417f749540b0c6dc5a
Csoti <csotimatyas93@gmail.com>
2022-02-16 14:28:07 +0200

    Update-2

--- venczelek/config/database.js
+++ venczelek/config/database.js
@@ -1,26 +1,26 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "venczelek"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"),
-      ssl: env.bool("DATABASE_SSL", false),
-    },
-  },
-});
 // module.exports = ({ env }) => ({
 //   connection: {
-//     client: "mysql",
+//     client: "postgres",
 //     connection: {
-//       host: env("DATABASE_HOST", "localhost"),
-//       port: env.int("DATABASE_PORT", 3306),
-//       database: env("DATABASE_NAME", "venczele_venczelek"),
-//       user: env("DATABASE_USERNAME", "venczele_J0c3r"),
-//       password: env("DATABASE_PASSWORD", "makitax1234"),
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "venczelek"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "postgres"),
 //       ssl: env.bool("DATABASE_SSL", false),
 //     },
 //   },
 // });
+module.exports = ({ env }) => ({
+  connection: {
+    client: "mysql",
+    connection: {
+      host: env("DATABASE_HOST", "venczelek.ro"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "venczele_venczelek"),
+      user: env("DATABASE_USERNAME", "venczele_J0c3r"),
+      password: env("DATABASE_PASSWORD", "makitax1234"),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+  },
+});
