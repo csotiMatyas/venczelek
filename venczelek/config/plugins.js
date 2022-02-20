fad8a639514648e3eda13f9360aad21c202731dd
Csoti <csotimatyas93@gmail.com>
2022-02-20 19:11:21 +0200

    another update

--- venczelek/config/plugins.js
+++ venczelek/config/plugins.js
@@ -1,18 +1,14 @@
-module.exports = ({ env }) => ({
-  upload: {
+module.exports = {
+  graphql: {
     config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {
-          folder: "venczelek",
-        },
-        delete: {},
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
       },
     },
   },
-});
+};
